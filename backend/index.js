3c1c153f536e41380c147565b9cfd2f545f7fc04
Sergio <sez1@gcloud.ua.es>
2021-03-01 15:46:38 +0100

    Chatbot integrado + un poco de api

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ require('dotenv').config();
 // Crear una aplicación de express
 const app = express();
 
+
 // llamar a nuestra cadena de conexion
 dbConnection();
 
@@ -31,6 +32,7 @@ app.use('/api/clientes', require('./routes/clientes.routes'));
 app.use('/api/login', require('./routes/auth.routes'));
 app.use('/api/verificacion', require('./routes/verificacion.routes'));
 app.use('/api/recuperar', require('./routes/password.routes'));
+app.use('/api/maya', require('./routes/maya.routes'));
 
 /*
 app.use('/api/usuarios', require('./routes/usuarios.routes'));
@@ -38,6 +40,11 @@ app.use('/api/login', require('./routes/auth.routes'));
 app.use('/api/grupos', require('./routes/grupos.routes'));
 */
 
+
+
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en el puerto ' + process.env.PORT);
 });
+
+
+
