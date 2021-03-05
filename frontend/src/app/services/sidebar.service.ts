4cdcc32851ec7791bbf7bf8c2e90469959470be5
Monica Iglesias <mig11@gcloud.ua.es>
2021-03-05 20:46:19 +0100

    sidebar arreglado version 2

--- frontend/src/app/services/sidebar.service.ts
+++ frontend/src/app/services/sidebar.service.ts
@@ -16,6 +16,7 @@ export class SidebarService {
   menuCliente: sidebarItem[]=[
     { titulo: 'Tu Perfil', icono: 'person', sub: false, url: '/perfil/cliente/'},
     { titulo: 'Prendas', icono: 'pages', sub: false, url: '/perfil/cliente/prendas'},
+    { titulo: 'Cambiar contraseña', icono: 'history_edu', sub: false, url: '/perfil/contra'},
   ];
   menuUsuario: sidebarItem[]=[
     { titulo: 'Perfil', icono: 'person', sub: false, url: '/perfil/usuario/'},
