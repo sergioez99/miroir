11cb00effd0c92dbd809a55fcd9ad9ed71823b34
paula-lv <plv11@gcloud.ua.es>
2021-03-10 12:41:42 +0100

    Pi funciona

--- frontend/src/app/pages/pruebasMotor/pruebas2/pruebas2.component.ts
+++ frontend/src/app/pages/pruebasMotor/pruebas2/pruebas2.component.ts
@@ -5,7 +5,6 @@ import { ECamera, ELight } from 'src/app/motorEngine/TEntity';
 import { TNode } from 'src/app/motorEngine/TNode';
 import { mat4, vec3, vec4 } from 'gl-matrix';
 import { TMotorTAG } from 'src/app/motorEngine/TMotorTAG';
-import { pi } from 'pi';
 import { htmlAstToRender3Ast } from '@angular/compiler/src/render3/r3_template_transform';
 
 
@@ -29,13 +28,13 @@ export class Pruebas2Component implements OnInit {
     //hay que crear antes la luz y la cámara que el personaje, no se podría dibujar el personaje si no se hace así
     this.motor = new TMotorTAG();
 
-    this.cam = this.motor.crearCamara(null, [0,0,25], [0,7*pi/4,0], [1,1,1], 0.1, 100, 0, 0, 0, 0, 0);
-    this.luz = this.motor.crearLuz(null, [0,25,0], [3*pi/2,0,0], [1,1,1], [1,1,1], "puntual", 1, 1, 1, 1, 1);
+    this.cam = this.motor.crearCamara(null, [0,0,25], [0,7*Math.PI/4,0], [1,1,1], 0.1, 100, 0, 0, 0, 0, 0);
+    this.luz = this.motor.crearLuz(null, [0,25,0], [3*Math.PI/2,0,0], [1,1,1], [1,1,1], "puntual", 1, 1, 1, 1, 1);
 
     this.personaje = this.motor.crearNodo(null, [10, 0, 10], [0,0,0],[1,1,1], null, null, null);
 
     this.cuerpo = this.motor.crearModelo(this.personaje, [0,0,0], [0,0,0], [1,1,1], "body.blend");
-    this.sombrero = this.motor.crearModelo(this.personaje, [0,10,0], [0,0,pi/8], [1,1,1], "hat.blend");
+    this.sombrero = this.motor.crearModelo(this.personaje, [0,10,0], [0,0,Math.PI/8], [1,1,1], "hat.blend");
     
 
   }
